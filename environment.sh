1cb59c32e926e5ae0fc16f8476de466cb028a5fb
YuehChuan <YuehChuan@users.noreply.github.com>
2016-10-03 15:48:48 +0800

    edit environment.sh

--- environment.sh
+++ environment.sh
@@ -1,6 +1,6 @@
 echo initial techman_robot shell environment
 source /opt/ros/indigo/setup.bash
-source /home/schwarmcyc/Documents/techman_robot/devel/setup.bash
+source /home/<YOUR-USER-NAME>/Documents/techman_robot/devel/setup.bash
 
 
 #FOR ORB_SLAM2 ROS
